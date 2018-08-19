0066a80dd6851a8109d619663ee7515fee54cc61
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2018-08-19 00:40:08 -0600

    Extra del graph, mostrar seleccionados

--- runall.sh
+++ runall.sh
@@ -19,7 +19,32 @@ echo -e "\r\n*** Generating Graphviz *** "
 echo -e "Processing File: \t src/gengraph.awk"
 echo -e "Input File: \t\t compose_output/composed_plan.txt"
 echo -e "Output File: \t\t gv_output/study_plan.pdf"
-awk -f src/gengraph.awk compose_output/composed_plan.txt | dot -Tpdf -o gv_output/study_plan.pdf
+###############################################################################################
+choice=""
+while [ "$choice" != "q" ]
+do
+        echo
+        echo "Seleccionar una opcion!"
+        echo "1) Todos los cursos"
+        echo "2) Cursos EIF"
+        echo "3) Cursos EIF y MAY"
+        echo "4) Cursos EIF y Optativos"
+        echo "q) Continuar con Prolog"
+        echo
+
+        read choice
+
+        case $choice in
+            '1') awk -f src/gengraph.awk compose_output/composed_plan.txt | dot -Tpdf -o gv_output/study_plan.pdf && firefox gv_output/study_plan.pdf ;;
+            '2') awk -f src/gengraph_EIF.awk compose_output/composed_plan.txt | dot -Tpdf -o gv_output/study_plan_EIF.pdf  && firefox gv_output/study_plan_EIF.pdf;;
+            '3') awk -f src/gengraph_EIFMAY.awk compose_output/composed_plan.txt | dot -Tpdf -o gv_output/study_plan_EIFMAY.pdf && firefox gv_output/study_plan_EIFMAY.pdf;;
+            '4') awk -f src/gengraph_EIFOPT.awk compose_output/composed_plan.txt | dot -Tpdf -o gv_output/study_plan_EIFOPT.pdf && firefox gv_output/study_plan_EIFOPT.pdf;;
+            'q') echo "Continuando con Prolog!";;
+            *)   echo "menu item is not available; try again!";;
+        esac
+done
+###############################################################################################
+
 #
 retVal=$?
 if [ $retVal -ne 0 ]; then
